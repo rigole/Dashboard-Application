d8c4290aa30052ee5b04271110f4d20d5c61596a
rigole <foplacide@gmail.com>
2023-07-24 21:28:24 +0100

    setting the backend properties

--- src/main/java/com/DashboardUsers/dashboardusers/repository/UserRepository.java
+++ src/main/java/com/DashboardUsers/dashboardusers/repository/UserRepository.java
@@ -1,5 +1,11 @@
 package com.DashboardUsers.dashboardusers.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.DashboardUsers.dashboardusers.model.User;
+
+public interface UserRepository extends JpaRepository<User,Long>{
+	
+	
+	
 }
