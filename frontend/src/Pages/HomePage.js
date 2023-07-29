6bd35b9973e61828fb3aa4efd4527d5a1743b57c
rigole <foplacide@gmail.com>
2023-07-29 16:28:29 +0100

    Fetching data from the backend

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+
 
 const HomePage = () => {
+
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        loadUsers()
+    }, [])
+
+    const loadUsers = async () => {
+        const result = await axios.get("http://localhost:8080/users");
+        setUsers(result.data);
+    }
+
     return (
         <div className='container'>
             <div className='py-4'>
-                <table className='table boder shadow'>
+                <table className='table border shadow'>
                     <thead>
                         <tr>
                             <th scope="col">#</th>
                             <th scope="col">First</th>
                             <th scope="col">Second</th>
+                            <th scope="col">Third</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th>1</th>
-                            <th>Test 1</th>
-                            <th>Test 2</th>
-                        </tr>
+                        {
+                            users.map((user, index) =>(
+                                <tr>
+                                <th scope="row" key={index}>{index + 1}</th>
+                                <th>{user.name}</th>
+                                <th>{user.username}</th>
+                                <th>{user.email}</th>
+                            </tr>
+                            ))
+                        }
+
+                       
                     </tbody>                
                 </table>
             </div>
--- src/main/java/com/DashboardUsers/dashboardusers/controller/UserController.java
+++ src/main/java/com/DashboardUsers/dashboardusers/controller/UserController.java
@@ -3,6 +3,7 @@ package com.DashboardUsers.dashboardusers.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import com.DashboardUsers.dashboardusers.model.User;
 import com.DashboardUsers.dashboardusers.repository.UserRepository;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
 	
 	@Autowired
