1cef506e5a33fbffd0ac412a7c03a20052d42ba4
rigole <foplacide@gmail.com>
2023-07-29 15:10:43 +0100

    adding the frontend with its main component(navbar, addUsers, edits users, views list)

---
+++ frontend/src/Layout/NavBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+
+const NavBar = () => {
+
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Dashboard</a>
+                    <button 
+                        className="navbar-toggler" 
+                        type="button" 
+                        data-bs-toggle="collapse"
+                        aria-controls=""
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                        >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <button className="btn btn-outline-light">Add User</button>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
